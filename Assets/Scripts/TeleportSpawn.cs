8aaa393304aac32b395a54736e22d6a485e98c2d
NoahNo <HallN8431@sduhsd.lan>
2016-05-10 13:00:50 -0700

    awd
    awd

---
+++ Assets/Scripts/TeleportSpawn.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleportSpawn : MonoBehaviour {
+	public int code;
+	float disableTimer=0;
+
+	void Update(){
+		if (disableTimer > 0)
+						disableTimer -= Time.deltaTime;
+	}
+
+	void OnTriggerEnter(Collider collider){
+		if (collider.gameObject.name == "FPSController" && disableTimer<=0) {
+
+			foreach(TeleportSpawn tp in FindObjectsOfType<TeleportSpawn>()){	
+
+				if(tp.code==code && tp !=this){
+					tp.disableTimer=10;
+					Vector3 position=tp.gameObject.transform.position;
+					position.y+=2;
+					collider.gameObject.transform.position=position;
+				
+				}
+
+
+			}
+
+
+				}
+	}
+
+}
